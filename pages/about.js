968291ff60af2103ad5076a154b2355ca3f00857
Brad Garropy <bradgarropy@gmail.com>
2019-06-10 23:01:02 -0500

    include seo on pages.

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Meta from "../components/SEO/Meta"
+import Facebook from "../components/SEO/Facebook"
+import Twitter from "../components/SEO/Twitter"
 
 const aboutPage = () => {
     return (
         <Layout>
+            <Meta title="about"/>
+            <Facebook/>
+            <Twitter/>
+
             <p>This is the about page</p>
         </Layout>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Meta from "../components/SEO/Meta"
+import Facebook from "../components/SEO/Facebook"
+import Twitter from "../components/SEO/Twitter"
 
 const indexPage = () => (
     <Layout>
+        <Meta title="next starter"/>
+        <Facebook/>
+        <Twitter/>
+
         <p>Hello Next.js</p>
     </Layout>
 )
