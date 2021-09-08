b1de26cee13fdb589274a3d51e22cad060fee894
Brad Garropy <bradgarropy@gmail.com>
2021-09-07 21:45:27 -0500

    add return to index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import SEO from "@bradgarropy/next-seo"
 import Layout from "components/Layout"
 import {FC} from "react"
 
-const IndexPage: FC = () => (
-    <Layout>
-        <SEO title="next starter" />
-        <p>home</p>
-    </Layout>
-)
+const IndexPage: FC = () => {
+    return (
+        <Layout>
+            <SEO title="next starter" />
+            <p>home</p>
+        </Layout>
+    )
+}
 
 export default IndexPage
