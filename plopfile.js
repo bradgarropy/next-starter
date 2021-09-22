5279705fa942963f5d119104c64c0046236573e8
Brad Garropy <bradgarropy@gmail.com>
2021-09-21 19:54:00 -0500

    add react context generator.

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,7 @@
 const config = plop => {
     plop.load("@bradgarropy/plop-generator-react-component")
     plop.load("@bradgarropy/plop-generator-react-hook")
+    plop.load("@bradgarropy/plop-generator-react-context")
 }
 
 module.exports = config
